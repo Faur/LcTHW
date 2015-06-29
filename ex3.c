b2b5e2f9446a9d5d1e8c6e1190f93e288461315e
Toke Faurby <tokeee@gmail.com>
2015-06-29 18:35:26 +0200

    Initial answers to question 3

---
+++ ex3.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // REMEMBER .h
+
+int main (){
+	int age = 23;
+	int height = 178;
+
+	printf("I am %d years old.\n", age); // REMEMBER ; 
+	printf("And %d cm tall.\n", height); // REMEMBER ;
+
+	return 0; // REMEMBER to return
+}
