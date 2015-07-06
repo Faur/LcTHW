e4053923a38a3b381fd76dca0ce5d218799418d0
A Happy User <auser@nothing.none>
2015-07-06 11:37:56 -0700

    Add new exercises

---
+++ ex5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*This is a comment*/
+int main(int argc, char **argv) {
+  distance = 100;
+  
+  //This is also a comment
+  printf("I ran %d miles.\n", distance);
+
+  return 0;
+}
